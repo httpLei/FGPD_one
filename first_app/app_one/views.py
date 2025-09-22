22ae2d9af8c33224977248413d17007550b8692c
lei <lerahcaones18@gmail.com>
2025-09-22 20:29:56 +0800

    feature(app_one): complete app setup

--- first_app/app_one/views.py
+++ first_app/app_one/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
+import random
 
 def home(request):
-    return render(request, 'app_one/home.html')
+    quotes = [
+        "Keep going, you're doing great!",
+        "Believe in yourself.",
+        "One step at a time.",
+        "Stay positive, work hard, make it happen.",
+        "Don’t stop until you’re proud."
+    ]
+
+    random_colors = {
+        "Red": "red",
+        "Orange": "orange",
+        "Yellow": "yellow",
+        "Green": "green",
+        "Blue": "blue",
+        "Indigo": "indigo",
+        "Violet": "violet",
+    }
+
+    selected_quote = None
+    selected_color = None
+
+    if request.method == "POST":
+        selected_color = request.POST.get("color")
+        selected_quote = random.choice(quotes)
+
+    return render(request, 'app_one/home.html', {
+        "quote": selected_quote,
+        "color": selected_color,
+        "colors": random_colors
+    })
